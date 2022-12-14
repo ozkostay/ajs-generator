4029b995d1eabf1e27ecc66ba0063606216d41dc
ozkostay <ozkostay@mail.ru>
2022-12-14 19:39:09 +0500

    ajs-gen 141222 -1

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,13 +1,15 @@
 export default class Team {
   constructor() {
     this.enemies = [];
+    this.iterator = this[Symbol.iterator]();
   }
 
   add(item) {
     this.enemies.push(item);
+    this.iterator = this[Symbol.iterator]();
   }
 
-  * iterator() {
+  * [Symbol.iterator]() {
     let index = 0;
     const enemies = [...this.enemies];
     while (index < enemies.length) {
@@ -16,16 +18,7 @@ export default class Team {
     }
   }
 
-  // * [Symbol.iterator]() {
-  //   let index = 0;
-  //   const enemies = [...this.enemies];
-  //   while (index < enemies.length) {
-  //     yield enemies[index];
-  //     index += 1;
-  //   }
-  // }
-
-  getOneEnemy(iter) {
-    console.log(iter.next().value);
+  getOneEnemy() {
+    return this.iterator.next().value;
   }
 }
